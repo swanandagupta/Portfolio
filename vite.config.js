526bfe81e7a927821c82b67ea919e43c6de10dcc
Swananda Gupta <swananda.gupta@gmail.com>
2026-08-07 14:18:12 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   base: "/Portfolio/",
-  plugins: [react(), viteSingleFile()],
-  build: {
-    cssCodeSplit: false,
-    assetsInlineLimit: 100000000,
-  },
+  plugins: [react()],
 });
